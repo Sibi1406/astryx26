b8aa9858d5f86ce085c1a871db698ac9e2c02340
Sobika Joleen <joleensobika05@gmail.com>
2026-03-13 13:18:23 +0530

    ....

--- src/Home.js
+++ src/Home.js
@@ -196,7 +196,7 @@ export default function Home() {
   className="register-btn"
   whileHover={{ scale: 1.08 }}
   whileTap={{ scale: 0.95 }}
-  onClick={() => setShowRegistrationForm(true)}
+  onClick={() => window.alert("Registration Closed")}
 >
   Register Now
 </motion.button>
